0df96400950d20d61777c0bae336535ee1b87eb3
Edenilson Alves <edenilson.alves@ammovarejo.com.br>
2023-05-31 22:34:41 -0300

    adiciconando casos de testes novos

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -11,29 +11,72 @@ describe('Esperas...', () => {
     it('Deve aguardar elemento está disponivel', () => {
         cy.get('#buttonDelay').click()
         cy.get('#novoCampo').type('funciona')
-        
-        
+    })
+    it('Deve fazer retrys', () => {
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo')
+            .should('exist')
+            .type('funciona')
     })
 
+    it('Uso so find', () => {
+        cy.get('#buttonList').click()
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1')
+        cy.get('#lista li span')
+            .should('contain', 'Item 2')
+    })
 
+    it('Uso do timeout', () => {
+        /* cy.get('#buttonDelay').click()
+           cy.get('#NovoCampo', { timeout: 1000 }).should('exist')    timeout vai esperar  ate 30 segundo para executar, caso execute antes ele segue 
+   
+           cy.get('#buttonListDOM').click()
+           cy.wait(5000)                                               wait vai esperar o tempo informado independente se ser executado antes 
+           cy.get('#lista li span', { timeout: 3000 })
+               .should('contain', 'Item 2')*/
 
+        cy.get('#buttonListDOM').click()
+        cy.get('#lista li span')
+            .should('have.length', 1)
+        cy.get('#lista li span')
+            .should('have.length', 2)
 
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    it.only('Click retry', () => {
+        cy.get('#buttonCount')
+            .click()
+            .click()
+            .should('have.value', '111')
+    })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
